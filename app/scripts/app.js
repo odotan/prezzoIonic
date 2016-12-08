f093c2bac4883e44a163c01d9507b79bc876f2c6
Amit Merla <amitmerla@Amits-MacBook-Pro.local>
2016-12-09 00:44:08 +0200

    added stateprovider angular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,5 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name testAppApp
- * @description
- * # testAppApp
- *
- * Main module of the application.
- */
 angular
   .module('testAppApp', [
     'ngAnimate',
@@ -15,21 +7,56 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'ui.router'
   ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+  .config(function ($stateProvider, $urlRouterProvider) {
+
+      $stateProvider
+    
+        // route to show our basic form (/form)
+        .state('form', {
+            url: '/form',
+            templateUrl: '../views/registrationForm.html',
+            controller: 'formController'
+        })
+        
+        // nested states 
+        // each of these sections will have their own view
+        // url will be nested (/form/profile)
+        .state('form.profile', {
+            url: '/profile',
+            templateUrl: '../views/form-profile.html'
+        })
+        
+        // url will be /form/interests
+        .state('form.interests', {
+            url: '/interests',
+            templateUrl: '../views/form-interests.html'
+        })
+        
+        // url will be /form/payment
+        .state('form.payment', {
+            url: '/payment',
+            templateUrl: '../views/form-submit.html'
+        });
+       
+    // catch all route
+    // send users to the form page 
+    $urlRouterProvider.otherwise('/form/profile');
+})
+
+// our controller for the form
+// =============================================================================
+.controller('formController', function($scope) {
+    
+    // we will store all of our form data in this object
+    $scope.formData = {};
+    
+    // function to process the form
+    $scope.processForm = function() {
+        alert('awesome!');  
+    };
+    
+});
+
